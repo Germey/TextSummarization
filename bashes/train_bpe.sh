88d1b90a74a63660a56430c74323c9f72dba3bcc
Germey <cqc@cuiqingcai.com>
2018-04-01 21:03:27 +0800

    add bashes

--- bashes/train_bpe.sh
+++ bashes/train_bpe.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+cd ..
 python3 train.py --model_dir model/bpe --source_vocabulary dataset/nlpcc_bpe/vocab.json --target_vocabulary dataset/nlpcc_bpe/vocab.json --source_train_data dataset/nlpcc_bpe/articles.train.txt --target_train_data dataset/nlpcc_bpe/summaries.train.txt --source_valid_data dataset/nlpcc_bpe/articles.eval.txt --target_valid_data dataset/nlpcc_bpe/summaries.eval.txt --num_encoder_symbols 21549 --num_decoder_symbols 21549 | tee logs/bpe.log
