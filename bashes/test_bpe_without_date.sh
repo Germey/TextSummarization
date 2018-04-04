674ed0b1b9094493481dd4285dcba33ec5d25cbf
Germey <cqc@cuiqingcai.com>
2018-04-04 16:38:13 +0800

    add cd

--- bashes/test_bpe_without_date.sh
+++ bashes/test_bpe_without_date.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+cd ..
 python3 decode.py --gpu 2 --model_path model/bpe_without_date/summary.ckpt-30100 --decode_input dataset/nlpcc_bpe_without_date/articles.test.txt --decode_output dataset/nlpcc_bpe_without_date/summaries.test.txt
